04c28061872afe748446cfb2fcdd03af8c90699d
Stolarskis <csstolarski@gmail.com>
2020-03-20 23:40:05 -0400

    added integration with a postgres database

--- api/routes/dog.js
+++ api/routes/dog.js
@@ -1,17 +1,30 @@
 import express from "express";
+import models from "../database/models";
 
 let app = express.Router();
 
 app.get("/all", (req, res) => {
-  res.status(200).send({
-    message: "return all dogs"
+  models.Dogs.findAll().then(dogs => {
+    res.status(200).send({
+      message: "Returned all dogs",
+      body: dogs
+    });
   });
 });
 
 // get a single dog by id
 app.get("/:id", (req, res) => {
-  res.status(200).send({
-    message: "Return dog with id " + req.params.id
+  getDog(req.params.id).then(dog => {
+    if (dog === null) {
+      res.status(404).send({
+        message: "Dog not found",
+        body: {}
+      });
+    } else {
+      res.status(200).send({
+        body: dog
+      });
+    }
   });
 });
 
@@ -24,9 +37,16 @@ app.get("/:id/vaccRecord", (req, res) => {
 
 //Add Dog
 app.post("/", (req, res) => {
-  res.status(200).send({
-    message: "Dog Added",
-    body: req.body
+  //Create Dog
+  models.Dogs.create({
+    name: req.body.name,
+    breed: req.body.breed,
+    owner: req.body.owner
+  }).then(dog => {
+    res.status(201).send({
+      message: "Dog Added",
+      body: dog
+    });
   });
 });
 
@@ -38,8 +58,16 @@ app.post("/:id/vaccRecord", (req, res) => {
 });
 
 app.delete("/:id", (req, res) => {
-  res.status(200).send({
-    message: "Deleted Dog " + req.params.id
+  models.Dogs.destroy({ where: { id: req.params.id } }).then(result => {
+    if (result === 0) {
+      res.status(404).send({
+        message: "Dog not found"
+      });
+    } else {
+      res.status(200).send({
+        message: "Dog deleted successfully"
+      });
+    }
   });
 });
 
@@ -50,9 +78,15 @@ app.delete("/:id/vaccRecord", (req, res) => {
 });
 
 app.put("/:id", (req, res) => {
-  res.status(200).send({
-    message: "Updated dog " + req.params.id,
-    body: req.body
+  models.Dogs.update(
+    { name: req.body.name, breed: req.body.breed, owner: req.body.owner },
+    { where: { id: req.params.id } }
+  ).then(result => {
+    if (result === 1) {
+      res.status(404).send({ message: "Failed to update dog" });
+    } else {
+      res.status(200).send({ message: "Updated Dog" });
+    }
   });
 });
 
@@ -62,5 +96,9 @@ app.put("/:id/vaccRecord", (req, res) => {
     body: req.body
   });
 });
+
+function getDog(id) {
+  return models.Dogs.findOne({ where: { id: id } });
+}
 
 module.exports = app;
