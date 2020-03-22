532b80413b1de22b694f8407cfacec2bb90ac7f9
Stolarskis <csstolarski@gmail.com>
2020-03-22 16:32:28 -0400

    added router, to point to contact page

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./pages/App";
+import Contact from "./pages/Contact";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
