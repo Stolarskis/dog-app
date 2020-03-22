04bdd7b78bee673182258b2b9368ae235f83a2bd
Stolarskis <csstolarski@gmail.com>
2020-03-22 02:14:34 -0400

    inital commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Dogs from "./Dogs.js";
 
--- client/src/Dog.js
+++ client/src/Dog.js
@@ -1,14 +1,82 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
 
 const Dog = ({ dogInfo }) => {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  function deleteDog() {
+    fetch(`http://localhost:9000/dog/${dogInfo.id}`, {
+      method: "DELETE",
+      mode: "cors"
+    });
+  }
+
+  /**
+  function updateDog() {
+    fetch(`http://localhost:9000/dog/${dogInfo.id}`, {
+      method: "PUT",
+      mode: "cors",
+       headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+
+    });
+  }
+  */
   return (
-    <div>
-      <p>{dogInfo.name}</p>
-      <p>{dogInfo.breed}</p>
-      <p>{dogInfo.owner}</p>
-      <p>{dogInfo.sex}</p>
-      <p>{dogInfo.weight}</p>
-    </div>
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        ></Typography>
+        <Typography variant="h5">{dogInfo.name}</Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {dogInfo.breed}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {dogInfo.owner}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained">EDIT</Button>
+        <Button variant="contained">GET INFO</Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            deleteDog(dogInfo.id);
+          }}
+        >
+          DELETE
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
