b36ecfeb67c14712b87cb635c68b71257856546a
Stolarskis <csstolarski@gmail.com>
2020-03-22 23:55:36 -0400

    Users can now add dogs to the database

--- client/src/pages/AddDog.js
+++ client/src/pages/AddDog.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PageTabs from "../components/PageTabs";
+import DogForm from "../components/DogForm";
+import Typography from "@material-ui/core/Typography";
 
 const AddDog = () => {
   return (
     <div>
       <PageTabs tabNumber={1} />
-      <h1>Add Dog</h1>
+      <Typography variant="h4" component="h2" gutterBottom>
+        Add Dog
+      </Typography>
+      <DogForm />
     </div>
   );
 };
