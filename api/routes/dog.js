48d253616e92da7d8c93c4d3b4ee380f924572de
Stolarskis <csstolarski@gmail.com>
2020-04-05 23:07:33 -0400

    all routes now use async instead of then.

--- api/routes/dog.js
+++ api/routes/dog.js
@@ -3,66 +3,67 @@ import models from "../database/models";
 
 let app = express.Router();
 
-app.get("/all", (req, res) => {
-  models.Dogs.findAll().then(dogs => {
-    res.status(200).send({
-      message: "Returned all dogs",
-      body: dogs
-    });
+// Get all dogs
+app.get("/all", async (req, res) => {
+  console.log("Getting all dogs");
+  const dogs = await models.Dogs.findAll();
+  res.status(200).send({
+    body: dogs,
   });
 });
 
-// get a single dog by id
-app.get("/:id", (req, res) => {
-  getDog(req.params.id).then(dog => {
-    if (dog === null) {
-      res.status(404).send({
-        message: "Dog not found",
-        body: {}
-      });
-    } else {
-      res.status(200).send({
-        body: dog
-      });
-    }
-  });
+// Get a single dog by id
+app.get("/:id", async (req, res) => {
+  const dog = await getDog(req.params.id);
+  if (dog === null) {
+    res.status(404).send({
+      message: "Dog not found",
+    });
+  } else {
+    res.status(200).send({
+      body: dog,
+    });
+  }
 });
 
 //Add Dog
-app.post("/", (req, res) => {
-  //Create Dog
-  models.Dogs.create({
+app.post("/", async (req, res) => {
+  const dog = await models.Dogs.create({
     name: req.body.name,
     breed: req.body.breed,
     sex: req.body.sex,
     fixed: req.body.fixed,
     weight: req.body.weight,
     age: req.body.age,
-    owner: req.body.owner
-  }).then(dog => {
-    res.status(201).send({
-      message: "Dog Added",
-      body: dog
+    owner: req.body.owner,
+  });
+  res.status(201).send({
+    message: "Dog Added",
+    body: dog,
+  });
+});
+
+app.delete("/:id", async (req, res) => {
+  const result = await models.Dogs.destroy({ where: { id: req.params.id } });
+  if (result === 0) {
+    res.status(404).send({
+      message: "Dog not found",
     });
-  });
+  } else {
+    res.status(200).send({
+      message: "Dog deleted successfully",
+    });
+  }
 });
 
-app.delete("/:id", (req, res) => {
-  models.Dogs.destroy({ where: { id: req.params.id } }).then(result => {
-    if (result === 0) {
-      res.status(404).send({
-        message: "Dog not found"
-      });
-    } else {
-      res.status(200).send({
-        message: "Dog deleted successfully"
-      });
-    }
-  });
-});
-
-app.put("/:id", (req, res) => {
-  models.Dogs.update(
+app.put("/:id", async (req, res) => {
+  const dog = await getDog(req.params.id);
+  console.log("DOGGGGG", dog);
+  if (dog === null) {
+    res.status(404).send({ message: "Dog does not exist" });
+    return;
+  }
+  const result = await models.Dogs.update(
     {
       name: req.body.name,
       breed: req.body.breed,
@@ -70,91 +71,102 @@ app.put("/:id", (req, res) => {
       fixed: req.body.fixed,
       weight: req.body.weight,
       age: req.body.age,
-      owner: req.body.owner
+      owner: req.body.owner,
     },
     { where: { id: req.params.id } }
-  ).then(result => {
-    if (result === 1) {
-      res.status(404).send({ message: "Failed to update dog" });
-    } else {
-      res.status(200).send({ message: "Updated Dog" });
-    }
-  });
+  );
+  console.log(result);
+  if (result === 1) {
+    res.status(503).send({ message: "Failed to update dog" });
+  } else {
+    res.status(200).send({ message: "Updated Dog" });
+  }
 });
 
-app.get("/:id/vaccRecord", (req, res) => {
-  getVaccRecord(req.params.id).then(vaccRecord => {
-    if (vaccRecord === null) {
-      res.status(404).send({
-        message: "Vaccination record not found",
-        body: {}
-      });
-    } else {
-      res.status(200).send({
-        body: vaccRecord
-      });
-    }
-  });
+app.get("/:id/vaccRecord", async (req, res) => {
+  const vaccRecord = await getVaccRecord(req.params.id);
+  if (vaccRecord === null) {
+    res.status(404).send({
+      message: "Vaccination record not found",
+    });
+  } else {
+    res.status(200).send({
+      body: vaccRecord,
+    });
+  }
 });
 
-app.get("/:id/vaccRecord", (req, res) => {
-  res.status(200).send({
-    message: "Return dog's vaccination record with id " + req.params.id
-  });
+app.get("/:id/vaccRecord", async (req, res) => {
+  const dog = await getDog(req.params.id);
+  if (dog === null) {
+    res.status(404).send({
+      message: "Unable to post Vaccination Record. Dog does not exist",
+    });
+  } else {
+    res.status(200).send({
+      body: dog,
+    });
+  }
 });
 
 //this is absolute crap
-app.post("/:id/vaccRecord", (req, res) => {
+app.post("/:id/vaccRecord", async (req, res) => {
   //Check is given id exists for a dog in database
-  getDog(req.params.id).then(dog => {
-    if (dog === null) {
-      res.status(404).send({
-        message: "Unable to post Vaccination Record. Dog does not exist"
-      });
-    } else {
-      //Wait to check if dog exists, if it does, then check if vaccination record exists.
-      getVaccRecord(req.params.id).then(vaccRecord => {
-        if (vaccRecord !== null) {
-          res.status(404).send({
-            message: "Unable to post Vaccination Record. Record already exists"
-          });
-          return;
-        } else {
-          //Wait to check if vaccination record exists, if it doesn't then post vaccination record.
-          models.VaccRecord.create({
-            DogId: req.params.id,
-            rabiesDueDate: req.body.rabiesDueDate,
-            heartwormDueDate: req.body.heartwormDueDate,
-            fleaTickDueDate: req.body.fleaTickDueDate
-          }).then(result => {
-            res.status(200).send({
-              message: "Vaccination Record Added for dog " + req.params.id,
-              body: result
-            });
-          });
-        }
-      });
-    }
-  });
-});
-
-app.put("/:id/vaccRecord", (req, res) => {
-  models.VaccRecord.update({
+  const dog = await getDog(req.params.id);
+  if (dog === null) {
+    res.status(404).send({
+      message: "Unable to post Vaccination Record. Dog does not exist",
+    });
+    return;
+  }
+  let vaccRecord = await getVaccRecord(req.params.id);
+  if (vaccRecord !== null) {
+    res.status(400).send({
+      message: "Unable to post Vaccination Record. Record already exists",
+    });
+    return;
+  }
+  await models.VaccRecord.create({
     DogId: req.params.id,
     rabiesDueDate: req.body.rabiesDueDate,
     heartwormDueDate: req.body.heartwormDueDate,
-    fleaTickDueDate: req.body.fleaTickDueDate
-  }).then(result => {
-    res.status(200).send({
-      message: "Updated Vaccination record for dog " + req.params.id,
-      body: result
-    });
+    fleaTickDueDate: req.body.fleaTickDueDate,
+  });
+
+  res.status(200).send({
+    message: "Vaccination Record Added for dog " + req.params.id,
   });
 });
 
-app.delete("/:id/vaccRecord", (req, res) => {
+app.put("/:id/vaccRecord", async (req, res) => {
+  const dog = await getDog(req.params.id);
+  if (dog === null) {
+    res.status(404).send({
+      message: "Unable to update Vaccination Record. Dog does not exist",
+    });
+    return;
+  }
+  let vaccRecord = await getVaccRecord(req.params.id);
+  if (vaccRecord === null) {
+    res.status(400).send({
+      message: "Unable to update Vaccination Record. Record doesn't exist",
+    });
+    return;
+  }
+  const vaccRecordUpdate = await models.VaccRecord.update(
+    {
+      DogId: req.params.id,
+      rabiesDueDate: req.body.rabiesDueDate,
+      heartwormDueDate: req.body.heartwormDueDate,
+      fleaTickDueDate: req.body.fleaTickDueDate,
+    },
+    { where: { DogId: req.params.id } }
+  );
+
+  console.log(vaccRecordUpdate);
+
   res.status(200).send({
-    message: "Deleted vaccination record for dog " + req.params.id
+    message: "Updated Vaccination Record for dog " + req.params.id,
   });
 });
 
