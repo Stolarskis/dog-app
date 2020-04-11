e4b6c489895e54d07829d243f39b1f9093581816
stolarskis <csstolarski@gmail.com>
2020-04-11 00:28:11 -0400

    removed info dog page. Dog info now rendered on dog cards

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import DogsPage from "./pages/DogsPage";
 import Contact from "./pages/Contact";
 import AddDog from "./pages/AddDog";
 import EditDog from "./pages/EditDog";
-import InfoDog from "./pages/InfoDog";
 import PageTabs from "./components/PageTabs";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
@@ -15,7 +14,6 @@ const routing = (
       <Route exact path="/" component={DogsPage} />
       <Route path="/contact" component={Contact} />
       <Route path="/editDog/:id" component={EditDog} />
-      <Route path="/infoDog/:id" component={InfoDog} />
       <Route path="/addDog" component={AddDog} />
     </div>
   </Router>
