dda0534ea1df983792e48106545f6fe717c1a30b
stolarskis <csstolarski@gmail.com>
2020-04-11 01:00:18 -0400

    removed uneeded dependency

--- client/src/components/Dog.js
+++ client/src/components/Dog.js
@@ -11,7 +11,6 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import DogNotFound from "../../public/dognotfound.png";
 import Divider from "@material-ui/core/Divider";
 import DogTable from "./DogTable";
